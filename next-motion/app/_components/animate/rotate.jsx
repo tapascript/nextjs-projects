fc5716776389f967e8bc941f7581c6ff42f3cc4b
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-01-21 16:36:29 +0530

    Project README next-rotate

--- next-motion/app/_components/animate/rotate.jsx
+++ next-motion/app/_components/animate/rotate.jsx
@@ -6,9 +6,9 @@ const Rotate = () => {
     <motion.div
       animate={{ rotate: 360 }}
       transition={{ duration: 3 }}
-      className="w-[200px] h-[200px] bg-yellow-600 rounded m-2"
+      className="w-[200px] h-[200px] bg-purple-300 border border-purple-700 rounded m-2"
     >
-      <h2 className="text-center p-10 text-xl">I can Rotate</h2>
+      <h2 className="text-center p-4 text-3xl text-purple-900">Glad to See You!</h2>
     </motion.div>
   );
 };
--- next-motion/app/page.js
+++ next-motion/app/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 import Rotate from "./_components/animate/rotate";
 export default function Home() {
   return (
-    <>
-      <Link className=" m-4 text-5xl underline flex justify-center" href="/blog">Welcome to tapaScript Blog</Link>
+    <div className="flex flex-col justify-center items-center">
+      <Link className=" m-10 text-5xl underline" href="/blog">Welcome to tapaScript Blog</Link>
       <Rotate />
-    </>
+    </div>
   );
 }
